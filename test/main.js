f12a03d7a8ae137a1558751e2248d975fcfbcf9b
ehmicky <ehmicky@gmail.com>
2019-08-25 17:38:53 +0200

    Initial commit

---
+++ test/main.js
@@ -0,0 +1,9 @@
+import test from 'ava'
+
+import timeResolution from '../src/main.js'
+
+test('Returns an integer', t => {
+  const resolution = timeResolution()
+
+  t.true(Number.isInteger(resolution))
+})
